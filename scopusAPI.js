89ee3032fc59701908cf516a4e5d0f7cb39ce453
Lucas Schmutz <51484205+schmutzlucas@users.noreply.github.com>
2022-01-03 17:06:03 +0100

    added functions descriptions and comments

--- scopusAPI.js
+++ scopusAPI.js
@@ -1,5 +1,5 @@
 /**
- * These functions are used to collect and add the metrics from Scopus.
+ * These functions are used to collect and add the metrics from Scopus to an author profile.
  * A first API call is made by using the ORCID, if it succeeds it checks if the author obtained from Scopus has been
  * affiliated with an Australian institution. If either of these two steps fail, it tries to obtain the Scopus EID by
  * making an API call with the first and last name of the author. Then checks if the returned author is affiliated
@@ -9,17 +9,24 @@
  * the keys to distribute the load.
  */
 
-
 import {writeToFile} from "./readWriteFile.js";
 import fetch from "node-fetch";
 
-
-const apiKeys = ['6ab3c2a01c29f0e36b00c8fa1d013f83'];
+const apiKeys = [
+    '6c5874ca0af03eea96af105221f99625',
+    '6bb5f3f175ae62472ba8143341a05b3e',
+    '6ab3c2a01c29f0e36b00c8fa1d013f83'];
 const waitInterval = 333 / apiKeys.length;
 
 var apiKeyIndex = 0;
 var wait = new Promise(r => setTimeout(r, waitInterval));
 
+/**
+ * This function makes the API calls, store intermediary results to files, controls the quality of the {metrics}
+ * and adds the metrics to the corresponding author profile.
+ * @param data formatted in the previous steps
+ * @returns {Promise<void>}
+ */
 async function addMetrics(data) {
     for (const i in data.authors) {
         let metrics = await scopusApiCalls(data.authors[i]);
@@ -36,18 +43,28 @@ async function addMetrics(data) {
     }
 }
 
+/**
+ * This functions verifies if the corresponding author as been affiliated with an Australian institution
+ * @param dump The complete response from the Scopus API when requesting author data by EID/ORCID
+ * @returns {boolean}
+ */
 function checkAustralian(dump) {
     return JSON.stringify(dump).includes('Australia');
 }
 
-
+/**
+ * This functions retrieves and organizes the metrics data from two different Scopus API calls
+ * @param author Complete profile of a lead-investigator
+ * @returns {Promise<{metrics}>} Formatted metrics as an object
+ */
 async function scopusApiCalls(author) {
+    // First API call
     let baseUrl = 'https://api.elsevier.com/content/author?orcid=';
     let scopusUrl = baseUrl + author.orcid + '&apikey=' + apiKeys[apiKeyIndex++ % apiKeys.length];
-
     await wait;
     let response = await fetch(scopusUrl, {method: 'GET', headers: {Accept: 'application/json'}});
     wait = new Promise(r => setTimeout(r, waitInterval));
+    // Organization of the metrics and second API call
     let metrics = {};
     if (response.ok) {
         let dump = await response.json();
@@ -76,9 +93,14 @@ async function scopusApiCalls(author) {
     return metrics
 }
 
-
+/**
+ * This function offers an alternate way of looking for an author when the search by ORCID was unsatisfactory.
+ * It first searches the eid of the author by first and last name (as obtained with the splitName function).
+ * @param author Complete profile of a lead-investigator
+ * @returns {Promise<{metrics}>}
+ */
 async function scopusEidApiCall(author) {
-
+    // First API call by first name and last name to retrieve eid
     let baseUrl = 'http://api.elsevier.com/content/search/author?apikey=';
     let scopusUrl = baseUrl + apiKeys[apiKeyIndex++ % apiKeys.length] + '&query=AUTHFIRST%28'
         + author.firstName + '%29+AND+AUTHLASTNAME%28' + author.lastName + '%29';
@@ -88,12 +110,13 @@ async function scopusEidApiCall(author) {
     let dump = await response.json();
     let eid = dump['search-results'].entry[0].eid
 
+    // Second API calls by EID
     baseUrl = 'https://api.elsevier.com/content/author?eid=';
     scopusUrl = baseUrl + eid + '&apikey=' + apiKeys[apiKeyIndex++ % apiKeys.length];
-
     await wait;
     response = await fetch(scopusUrl, {method: 'GET', headers: {Accept: 'application/json'}});
     wait = new Promise(r => setTimeout(r, waitInterval));
+    // Organization of the metrics and third API call
     let metrics = {};
     if (response.ok) {
         dump = await response.json();
@@ -118,7 +141,6 @@ async function scopusEidApiCall(author) {
     }
     return metrics
 }
-
 
 export {
     writeToFile,
